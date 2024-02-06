6c2a155e685034554ec01933c03311a61294ab5d
torresvictor100 <torresvictor100@gmail.com>
2024-02-06 16:49:32 -0300

    create order entity

---
+++ src/main/java/com/loomi/ecommerce/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.loomi.ecommerce.repository;
+
+import com.loomi.ecommerce.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
