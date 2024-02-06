9bc7202467d65661a6584cafd1804fcc7ceef9df
torresvictor100 <torresvictor100@gmail.com>
2024-02-06 17:04:40 -0300

    create relationship client and order

--- src/main/java/com/loomi/ecommerce/entity/Order.java
+++ src/main/java/com/loomi/ecommerce/entity/Order.java
@@ -1,8 +1,12 @@
 package com.loomi.ecommerce.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
@@ -18,8 +22,62 @@ public class Order {
     @SequenceGenerator(name = "order_sq", sequenceName = "order_sq", initialValue = 1, allocationSize = 1)
     private Long id;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(name = "client_id", nullable = false)
+    private Long clientId;
+
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "client_id", nullable = false, insertable = false, updatable = false)
+    @JsonIgnoreProperties({"orders"})
+    private Client client;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
     private Timestamp orderDate;
 
     private BigDecimal totalAmount;
 
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Timestamp getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Timestamp orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
 }
