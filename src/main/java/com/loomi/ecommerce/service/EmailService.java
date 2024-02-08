cf3b0ffce959d33644eb79dc89bc5bdb897e35bb
torresvictor100 <torresvictor100@gmail.com>
2024-02-08 16:05:17 -0300

    create emailservice

---
+++ src/main/java/com/loomi/ecommerce/service/EmailService.java
@@ -0,0 +1,29 @@
+package com.loomi.ecommerce.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendEmail(String recipient, String subject, String message) {
+        MimeMessage mailMessage = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mailMessage);
+        try {
+            helper.setTo(recipient);
+            helper.setSubject(subject);
+            helper.setText(message, true);
+            emailSender.send(mailMessage);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
