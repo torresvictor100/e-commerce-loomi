a3d0e08c7c8c4eb89d22233ee231af0f8d002b68
torresvictor100 <torresvictor100@gmail.com>
2024-02-08 09:30:32 -0300

    create controller shoppingcart and prderitemShopping

--- src/main/java/com/loomi/ecommerce/service/ShoppingCartService.java
+++ src/main/java/com/loomi/ecommerce/service/ShoppingCartService.java
@@ -1,11 +1,12 @@
 package com.loomi.ecommerce.service;
 
-import com.loomi.ecommerce.entity.SalesReport;
+import com.loomi.ecommerce.entity.ShoppingCart;
 import com.loomi.ecommerce.repository.ShoppingCartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ShoppingCartService {
@@ -13,7 +14,33 @@ public class ShoppingCartService {
     @Autowired
     private ShoppingCartRepository shoppingCartRepository;
 
-    public List<ShoppingCartRepository> findAll(){
+    public List<ShoppingCart> findAll(){
         return shoppingCartRepository.findAll();
+    }
+
+    public ShoppingCart save(ShoppingCart shoppingCart){
+        shoppingCart.setId(null);
+        return shoppingCartRepository.save(shoppingCart);
+    }
+
+    public ShoppingCart findById(Long id) {
+        Optional<ShoppingCart> optionalShoppingCart =  shoppingCartRepository.findById(id);
+        return optionalShoppingCart.orElse(null);
+    }
+
+    public ShoppingCart update(ShoppingCart shoppingCart) {
+        ShoppingCart shoppingCartFound = findById(shoppingCart.getId());
+        if (shoppingCartFound != null) {
+            return shoppingCartRepository.save(shoppingCart);
+        }else{
+            return shoppingCart;
+        }
+    }
+
+    public void deleteById(Long id) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setId(id);
+        shoppingCartRepository.delete(shoppingCart);
+
     }
 }
