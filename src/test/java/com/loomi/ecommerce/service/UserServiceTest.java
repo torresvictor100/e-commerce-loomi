4105699822ae26f494b417cdd126697c3c928348
torresvictor100 <torresvictor100@gmail.com>
2024-02-09 14:35:09 -0300

    tests

--- src/test/java/com/loomi/ecommerce/service/UserServiceTest.java
+++ src/test/java/com/loomi/ecommerce/service/UserServiceTest.java
@@ -27,48 +27,71 @@ class UserServiceTest {
     private UserRepository userRepository;
 
     @Autowired
-    @InjectMocks
     private UserService userService;
 
     private Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-
-    @BeforeEach
-    void setUo() {
-        MockitoAnnotations.initMocks(this);
-    }
 
     @Test
     @DisplayName("Test save method in UserService")
     void save() {
 
         User userToSave = new User();
-        userToSave.setName("John Doe");
-        userToSave.setEmail("john.doe@example.com");
+        userToSave.setName("John3 Doe");
+        userToSave.setEmail("john4.doe@example.com");
         userToSave.setPassword("123");
 
         User savedUser = new User();
-        savedUser.setId(1L);
-        savedUser.setName("John Doe");
-        savedUser.setEmail("john.doe@example.com");
+        savedUser.setName("John2 Doe");
+        savedUser.setEmail("john2.doe@example.com");
         userToSave.setPassword("123");
         userToSave.setCreationDate(timestamp);
         userToSave.setCreationDate(timestamp);
         userToSave.setType(UserType.ADMIN);
         userToSave.setClient(null);
-
-        when(userRepository.save(userToSave)).thenReturn(savedUser);
 
         User result = userService.save(userToSave);
 
         assertThat(result).isNotNull();
-        assertThat(result.getId()).isEqualTo(1L);
-        assertThat(result.getName()).isEqualTo("John Doe");
-        assertThat(result.getEmail()).isEqualTo("john.doe@example.com");
+        assertThat(result.getName()).isEqualTo("John3 Doe");
+        assertThat(result.getEmail()).isEqualTo("john4.doe@example.com");
         userToSave.setCreationDate(timestamp);
         userToSave.setCreationDate(timestamp);
         userToSave.setType(UserType.ADMIN);
         userToSave.setClient(null);
 
+    }
+
+    @Test
+    void find() {
+
+        User userToSave = new User();
+        userToSave.setId(1L);
+        userToSave.setName("John Doe");
+        userToSave.setEmail("john.doe@example.com");
+        userToSave.setPassword("123");
+        userToSave.setCreationDate(timestamp);
+        userToSave.setCreationDate(timestamp);
+        userToSave.setType(UserType.ADMIN);
+        userToSave.setClient(null);
+        userService.save(userToSave);
+        User results = userService.findById(userToSave.getId());
+        assertThat(results).isNotNull();
+    }
+
+    @Test
+    void findNoFind() {
+
+        User userToSave = new User();
+        userToSave.setId(1L);
+        userToSave.setName("John Doe");
+        userToSave.setEmail("john.doe@example.com");
+        userToSave.setPassword("123");
+        userToSave.setCreationDate(timestamp);
+        userToSave.setCreationDate(timestamp);
+        userToSave.setType(UserType.ADMIN);
+        userToSave.setClient(null);
+        User results = userService.findById(userToSave.getId());
+        assertThat(results).isNull();
     }
 
 }
