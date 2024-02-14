e056ed3a8c4465f75108383235e082f5c2515ab8
torresvictor100 <torresvictor100@gmail.com>
2024-02-14 13:36:41 -0300

    finalizing the password system

--- src/main/java/com/loomi/ecommerce/entity/DTO/RegisterDTO.java
+++ src/main/java/com/loomi/ecommerce/entity/DTO/RegisterDTO.java
@@ -2,5 +2,5 @@ package com.loomi.ecommerce.entity.DTO;
 
 import com.loomi.ecommerce.entity.UserType;
 
-public record RegisterDTO(String name, String email, String password) {
+public record RegisterDTO(String name, String email) {
 }
---
+++ src/main/java/com/loomi/ecommerce/entity/DTO/TokenRequestDTO.java
@@ -0,0 +1,4 @@
+package com.loomi.ecommerce.entity.DTO;
+
+public record TokenRequestDTO(String token, String newPassword){
+}
